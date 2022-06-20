389cd600bf9917146f547938e486466b63821f1c
abshaibu <shaibuabdulsalam56@gmail.com>
2022-06-20 19:47:06 +0100

    basic arithemetic calc

---
+++ app.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+    return a + b;
+}
+add(a, b);
+
+const subtract = (a, b) => {
+    return a - b;
+}
+subtract(a, b);
+
+const multiply = (a, b) => {
+    return a * b;
+}
+multiply(a, b);
+
+const divide = (a, b) => {
+    return a / b;
+}
+divide(a, b);
